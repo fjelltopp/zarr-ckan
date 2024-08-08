f2da8f1d09891c1854ace25e45a7e8a9db57b602
toavina <toavina@fjelltopp.org>
2024-08-08 14:32:12 +0300

    compile SASS file and run watcher on pod initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 # build js components & allow editing
+
+# Build fjelltopp theme sass files
+FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
+
+if [ -d "$FJELLTOPP_THEME" ]; then
+  echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
+  npm --prefix "$FJELLTOPP_THEME" run compile
+  npm --prefix "$FJELLTOPP_THEME" run watch &
+else
+  echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
+fi
