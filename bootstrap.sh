4e7f042777c41d0876a94c460d82ab7f3e7c2b73
toavina <toavina@fjelltopp.org>
2024-08-27 10:58:19 +0100

    compile SASS file and run watcher on pod initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,9 @@
 FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
 
 if [ -d "$FJELLTOPP_THEME" ]; then
-  if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
-    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling and start watching ..."
-    npm --prefix "$FJELLTOPP_THEME" run compile
-    npm --prefix "$FJELLTOPP_THEME" run watch &
-  else
-    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
-    npm --prefix "$FJELLTOPP_THEME" run compile
-  fi
+  echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
+  npm --prefix "$FJELLTOPP_THEME" run compile
+  npm --prefix "$FJELLTOPP_THEME" run watch &
 else
   echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
 fi
