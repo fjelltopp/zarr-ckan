41ee86858cb347c0093dd72cb6d6a26c6f11b50b
Toavina A. <toavinahajaina@gmail.com>
2024-08-27 12:57:15 +0300

    compile SASS file and run watcher on pod initialization for dev (#131)
    * compile SASS file and run watcher on pod initialization
    
    * A small change to the bootstrap.sh; now watch only triggers if the env CKAN_SITE_URL is http://zarr.minikube
    
    ---------
    
    Co-authored-by: toavina <toavina@fjelltopp.org>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 # build js components & allow editing
+
+# Build fjelltopp theme sass files
+FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
+
+if [ -d "$FJELLTOPP_THEME" ]; then
+  if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
+    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling and start watching ..."
+    npm --prefix "$FJELLTOPP_THEME" run compile
+    npm --prefix "$FJELLTOPP_THEME" run watch &
+  else
+    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
+    npm --prefix "$FJELLTOPP_THEME" run compile
+  fi
+else
+  echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
+fi
