f4df83f66f47d703725cefbef5f233c15928d48c
Chas Nelson <github@chasnelson.co.uk>
2024-10-08 11:47:30 +0100

    revert: turn fjelltopp-theme compiling back on

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,16 +6,16 @@ cd /usr/lib/ckan/ckanext-zarr/ckanext/zarr/react/ && \
     npm run build
 
 # Build fjelltopp theme sass files
-# FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
-# if [ -d "$FJELLTOPP_THEME" ]; then
-#   if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
-#     echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling and start watching ..."
-#     npm --prefix "$FJELLTOPP_THEME" run compile
-#     npm --prefix "$FJELLTOPP_THEME" run watch &
-#   else
-#     echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
-#     npm --prefix "$FJELLTOPP_THEME" run compile
-#   fi
-# else
-#   echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
-# fi
+FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
+if [ -d "$FJELLTOPP_THEME" ]; then
+  if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
+    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling and start watching ..."
+    npm --prefix "$FJELLTOPP_THEME" run compile
+    npm --prefix "$FJELLTOPP_THEME" run watch &
+  else
+    echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
+    npm --prefix "$FJELLTOPP_THEME" run compile
+  fi
+else
+  echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
+fi
