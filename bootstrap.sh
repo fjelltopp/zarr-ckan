be31c860696908b4880182ef5105425e4d4d274a
toavina <toavina@fjelltopp.org>
2024-11-13 14:58:09 +0300

    The theme is installed using a git tag

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,16 @@ cd /usr/lib/ckan/ckanext-zarr/ckanext/zarr/react/ && \
     npm run build
 
 # Build fjelltopp theme sass files
-FJELLTOPP_THEME="$CKAN_HOME/ckanext-fjelltopp-theme"
+FJELLTOPP_THEME="$CKAN_HOME/venv/src/ckanext-fjelltopp-theme"
 if [ -d "$FJELLTOPP_THEME" ]; then
+  cd $FJELLTOPP_THEME
   if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
     echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling and start watching ..."
-    npm --prefix "$FJELLTOPP_THEME" run compile
-    npm --prefix "$FJELLTOPP_THEME" run watch &
+    npm run compile
+    npm run watch &
   else
     echo "The Fjelltopp Theme $FJELLTOPP_THEME is enabled, compiling ..."
-    npm --prefix "$FJELLTOPP_THEME" run compile
+    npm run compile
   fi
 else
   echo "The Fjelltopp Theme $FJELLTOPP_THEME is not enabled."
