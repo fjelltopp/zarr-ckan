fa15153f0f91c5ae61a3fc4bba12a8af8453a01b
Chas Nelson <github@chasnelson.co.uk>
2024-12-05 12:32:28 +0000

    fix: point db check at RDS (#221)
    * fix: point DB to RDS
    
    * fix: make sure we check  CKAN_SQLALCHEMY_URL variable exists before using it

--- ckan-entrypoint.sh
+++ ckan-entrypoint.sh
@@ -42,18 +42,16 @@ write_config () {
 }
 
 # Wait for PostgreSQL
-while ! pg_isready -h db -U ckan; do
+if [ -z "$CKAN_SQLALCHEMY_URL" ]; then
+  abort "ERROR: no CKAN_SQLALCHEMY_URL specified in docker-compose.yml"
+fi
+while ! pg_isready -d "$CKAN_SQLALCHEMY_URL"; do
   sleep 1;
 done
 
 # If we don't already have a config file, bootstrap
 if [ ! -e "$CONFIG" ]; then
   write_config
-fi
-
-# Get or create CKAN_SQLALCHEMY_URL
-if [ -z "$CKAN_SQLALCHEMY_URL" ]; then
-  abort "ERROR: no CKAN_SQLALCHEMY_URL specified in docker-compose.yml"
 fi
 
 if [ -z "$CKAN_SOLR_URL" ]; then
