5e4f224fe52191db8d1dcffaa9a1cdc7d06380cc
Chas Nelson <github@chasnelson.co.uk>
2025-01-23 14:00:46 +0000

    feat: compile ckanext-file-uploader on init

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# Build fjelltopp file uploader js and css files
+FILE_UPLOADER="$CKAN_HOME/ckanext-file-uploader"
+if [ -d "$FILE_UPLOADER" ]; then
+  cd "$FILE_UPLOADER/react"
+  if [ "$CKAN_SITE_URL" = "http://zarr.minikube" ]; then
+    echo "The Fjelltopp CKAN Extension $FILE_UPLOADER is enabled, building and then watching ..."
+    npm run build
+    npm run dev &
+  else
+    echo "The Fjelltopp CKAN Extension $FILE_UPLOADER is enabled, building ..."
+    npm run build
+  fi
+else
+  echo "The Fjelltopp CKAN Extension $FILE_UPLOADER is not enabled."
+fi
+
 # Build fjelltopp theme sass files
 FJELLTOPP_THEME="$CKAN_VENV/src/ckanext-fjelltopp-theme"
 if [ -d "$FJELLTOPP_THEME" ]; then
